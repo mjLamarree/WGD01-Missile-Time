d33658416baaf429333967ed006bc3bc00e02b49
mjLamarree <67963551+mjLamarree@users.noreply.github.com>
2020-09-15 21:58:24 -0400

    Restored Project
    Restored project to the state it was in on the night of 9/14/2020

---
+++ Missile Time/Assets/Scripts/MissileBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileBehavior : MonoBehaviour
+{
+    public GameObject missileHead;
+    public GameObject missileTarget;
+
+    public float missileSpeed;
+
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        MissileMovement();
+        
+    }
+
+    public void MissileMovement()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, missileTarget.GetComponent<Transform>().position, missileSpeed * Time.deltaTime);
+    }
+
+
+
+}
