5b814626082a86149548289a3c4d7d7b1f445777
ProjectCBL <projectxcbl@gmail.com>
2020-09-18 19:13:00 -0400

    Created a script that pauses the game

---
+++ Missile Time/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject PauseScreen;
+    private bool isGamePaused = false;
+
+    private void Start()
+    {
+        Resume();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isGamePaused) { Pause(); } else { Resume(); }
+        }
+        
+    }
+
+    void Pause()
+    {
+        PauseScreen.SetActive(true);
+        Time.timeScale = 0.0f;
+        isGamePaused = true;
+    }
+
+    void Resume()
+    {
+        PauseScreen.SetActive(false);
+        Time.timeScale = 1.0f;
+        isGamePaused = false;
+    }
+
+}
