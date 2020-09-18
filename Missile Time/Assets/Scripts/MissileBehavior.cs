966ccd334f9e3e515b8edf5b2c399d3e3fd82c58
mjLamarree <67963551+mjLamarree@users.noreply.github.com>
2020-09-17 21:38:56 -0400

    Alot of work :P
    I updated the missile script, created basic missile spawners and created prefab variants of the missiles. The sprites don't work but the other aspcets seem too.

--- Missile Time/Assets/Scripts/MissileBehavior.cs
+++ Missile Time/Assets/Scripts/MissileBehavior.cs
@@ -8,23 +8,39 @@ public class MissileBehavior : MonoBehaviour
     public GameObject missileTarget;
 
     public float missileSpeed;
-
+    private float targetY;
+    private float targetX;
     void Start()
     {
-       
+        missileTarget = GameObject.FindGameObjectWithTag("Boss");
+        targetY = missileTarget.GetComponent<Transform>().position.y + Random.Range(-10f, 10f);
+        targetX = missileTarget.GetComponent<Transform>().position.x + Random.Range(-10f, 10f);
     }
 
     void Update()
     {
+
+
         MissileMovement();
         
     }
 
     public void MissileMovement()
     {
-        transform.position = Vector3.MoveTowards(transform.position, missileTarget.GetComponent<Transform>().position, missileSpeed * Time.deltaTime);
+
+        transform.position = 
+            Vector3.MoveTowards(transform.position, new Vector3(targetX, targetY, 0f), missileSpeed * Time.deltaTime);
+
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Surface"))
+        {
+            GetComponentInChildren<MissileExplosion>().StartCoroutine("MissileImpact");
+        }
+
+    }
 
 
 }
---
+++ Missile Time/Assets/Scripts/MissileExplosion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileExplosion : MonoBehaviour
+{
+    public Animator anim;
+    public GameObject missileBody;
+    public GameObject missileTailFlame;
+    public Component missileBehaviorScript;
+    void Start()
+    {
+        anim = missileBody.GetComponent<Animator>();
+        missileBehaviorScript = missileBody.GetComponent<MissileBehavior>();
+        StartCoroutine("MissileExplosionCountdown");
+    }
+
+    public IEnumerator MissileImpact()
+    {
+        StopCoroutine("MissileExplosionCountdown");
+        anim.Play("explosion");
+        Destroy(missileBehaviorScript);
+        Destroy(missileTailFlame);
+        Debug.Log("Tailflame dead");
+        yield return new WaitForSeconds(1);
+        Destroy(missileBody);
+    }
+
+    public IEnumerator MissileExplosionCountdown()
+    {
+        yield return new WaitForSeconds(10f);
+        StartCoroutine("MissileImpact");
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine("MissileImpact");
+    }
+}
