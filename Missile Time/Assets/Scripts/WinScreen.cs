e17463ce741c4f1865b38ef9b022aa3268549178
ProjectCBL <projectxcbl@gmail.com>
2020-09-19 17:23:39 -0400

    Added a win screen to win the player kills the boss

---
+++ Missile Time/Assets/Scripts/WinScreen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject monster;
+    public GameObject winScreen;
+    public GameObject scoreText;
+
+    private void Start()
+    {
+        winScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (monster == null && player != null)
+        {
+            scoreText.GetComponent<TextMeshProUGUI>().text = "Score: " + GetComponent<ScoreTracker>().scoreTracker.ToString();
+            winScreen.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            winScreen.SetActive(false);
+        }
+    }
+
+    public void ResetLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
