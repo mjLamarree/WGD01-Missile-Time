ae24f5c34fc1c6f733e9475458cdbe565dc89d1c
ProjectCBL <projectxcbl@gmail.com>
2020-09-18 21:16:49 -0400

    Created Game Over Screen functions to be called

---
+++ Missile Time/Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject gameOverScreen;
+
+    private void Start()
+    {
+        gameOverScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (player == null)
+        {
+            gameOverScreen.SetActive(true);
+        }
+        else
+        {
+            gameOverScreen.SetActive(false);
+        }
+    }
+
+    public void ResetLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
