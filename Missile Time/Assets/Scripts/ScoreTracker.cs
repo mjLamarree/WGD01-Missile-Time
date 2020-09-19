4794911a24d5449dc27f3fcc7331e3e4a683ab6a
ProjectCBL <projectxcbl@gmail.com>
2020-09-19 17:24:14 -0400

    Added a score tracker script to keep track of player's score

---
+++ Missile Time/Assets/Scripts/ScoreTracker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTracker : MonoBehaviour
+{
+
+    public int scoreTracker;
+    public GameObject player;
+    public GameObject scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreTracker = 0;
+        scoreText.GetComponent<TextMeshProUGUI>().text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        scoreText.GetComponent<TextMeshProUGUI>().text = scoreTracker.ToString();
+
+        if(player != null)
+        {
+            scoreTracker = player.GetComponent<PlayerController>().jumpScore;
+        }
+
+    }
+}
